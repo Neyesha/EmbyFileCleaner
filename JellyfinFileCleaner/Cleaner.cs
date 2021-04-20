cd7b3a237d9d4e514ed2ee284f3921480fe762c7
Neyesha <arylin.h@outlook.com>
2021-04-20 20:06:39 +0200

    to jellyfin

--- JellyfinFileCleaner/Cleaner.cs
+++ JellyfinFileCleaner/Cleaner.cs
@@ -3,11 +3,11 @@
     using Emby.ApiClient;
     using Emby.ApiClient.Cryptography;
     using Emby.ApiClient.Model;
-    using Model.Json;
     using MediaBrowser.Model.Dto;
     using MediaBrowser.Model.Entities;
     using MediaBrowser.Model.Logging;
     using MediaBrowser.Model.Querying;
+    using Model.Json;
     using System;
     using System.Linq;
     using System.Reflection;
@@ -23,46 +23,47 @@
         public Cleaner(Config config)
         {
             this.config = config;
-            this.apiClient = this.GetApiClientInstanceAsync(this.config).GetAwaiter().GetResult();
+            this.apiClient = GetApiClientInstanceAsync(this.config).GetAwaiter().GetResult();
         }
 
         public void Run()
         {
-            this.RunAsync().GetAwaiter().GetResult();
+            RunAsync().GetAwaiter().GetResult();
         }
 
         private async Task RunAsync()
         {
-            var userId = await this.GetUserIdByUsername(this.config.ConnectionInfo.Username);
-            var playedItems = (await this.GetItems(userId))
-                .Where(item => {
+            string userId = await GetUserIdByUsername(this.config.ConnectionInfo.Username);
+            var playedItems = (await GetItems(userId))
+                .Where(item =>
+                {
                     var lastPlayedDate = item.UserData?.LastPlayedDate;
                     return lastPlayedDate != null && lastPlayedDate < DateTime.Now.AddDays(-this.config.RemoveOlderThanDays);
-            })
-            .OrderBy(item => this.GetItemNameFormattedByType(item));
-            var validItems = playedItems.Where(this.IsNotIgnored);
-            var pickedCount = playedItems.Count();
-            var deletedCount = 0;
-            var failedCount = 0;
+                })
+            .OrderBy(item => GetItemNameFormattedByType(item));
+            var validItems = playedItems.Where(IsNotIgnored);
+            int pickedCount = playedItems.Count();
+            int deletedCount = 0;
+            int failedCount = 0;
 
             foreach (var item in validItems)
             {
-                if(this.config.IsTest)
+                if (this.config.IsTest)
                 {
-                    Logger.Info($"Picked - {this.GetItemNameFormattedByType(item)}");
+                    Logger.Info($"Picked - {GetItemNameFormattedByType(item)}");
                 }
-                else if(await this.TryDeleteAsync(item))
+                else if (await TryDeleteAsync(item))
                 {
-                    Logger.Info($"Deleted - {this.GetItemNameFormattedByType(item)}");
+                    Logger.Info($"Deleted - {GetItemNameFormattedByType(item)}");
                     deletedCount++;
                 }
                 else
                 {
-                    failedCount++;                                         
+                    failedCount++;
                 }
             }
 
-            this.SaveSummary(pickedCount, deletedCount, pickedCount - validItems.Count(), failedCount);
+            SaveSummary(pickedCount, deletedCount, pickedCount - validItems.Count(), failedCount);
         }
 
         private void SaveSummary(int pickedCount, int deletedCount, int ignoredCount, int failedCount)
@@ -79,7 +80,7 @@
         {
             try
             {
-                if(item.CanDelete ?? true)
+                if (item.CanDelete ?? true)
                 {
                     await this.apiClient.DeleteItemAsync(item.Id);
                     return true;
@@ -87,18 +88,18 @@
 
                 throw new InvalidOperationException("Item marked not to be deleted.");
             }
-            catch(Exception e)
+            catch (Exception e)
             {
-                Logger.Error($"Could not delete {this.GetItemNameFormattedByType(item)}: {e.Message}", e);
+                Logger.Error($"Could not delete {GetItemNameFormattedByType(item)}: {e.Message}", e);
                 return false;
             }
         }
 
         private string GetItemNameByType(BaseItemDto item)
         {
-            if(Enum.TryParse(item.Type, out ItemType itemType))
+            if (Enum.TryParse(item.Type, out ItemType itemType))
             {
-                switch(itemType)
+                switch (itemType)
                 {
                     case ItemType.Episode:
                         return item.SeriesName;
@@ -132,13 +133,13 @@
 
         private bool IsNotIgnored(BaseItemDto item)
         {
-            var isIgnoredListContains = this.config.IgnoreListContains.Any(name => this.GetItemNameByType(item).ToLower().Contains(name.ToLower()));
-            var isIgnoredEquals = this.config.IgnoreListEquals.Any(name => this.GetItemNameByType(item).ToLower() == name.ToLower());
-            var ignored = isIgnoredListContains || isIgnoredEquals;
+            bool isIgnoredListContains = this.config.IgnoreListContains.Any(name => GetItemNameByType(item).ToLower().Contains(name.ToLower()));
+            bool isIgnoredEquals = this.config.IgnoreListEquals.Any(name => GetItemNameByType(item).ToLower() == name.ToLower());
+            bool ignored = isIgnoredListContains || isIgnoredEquals;
 
-            if(ignored && config.PrintIgnored)
+            if (ignored && this.config.PrintIgnored)
             {
-                Logger.Info($"Ignored - {this.GetItemNameFormattedByType(item)}");
+                Logger.Info($"Ignored - {GetItemNameFormattedByType(item)}");
             }
 
             return ignored == false;
@@ -149,9 +150,9 @@
             var users = await this.apiClient.GetUsersAsync(new UserQuery());
 
             var user = users.SingleOrDefault(u => u.Name.ToLower() == username.ToLower());
-            if(user == null)
+            if (user == null)
             {
-                var message = $"Could not find a user for name {username}";
+                string message = $"Could not find a user for name {username}";
                 Logger.Error(message);
                 throw new ArgumentException(message);
             }
@@ -189,9 +190,9 @@
             }
             else
             {
-                var device = new Device { DeviceId=string.Empty, DeviceName=string.Empty };
+                var device = new Device { DeviceId = string.Empty, DeviceName = string.Empty };
                 client = new ApiClient(logger, configLocal.ConnectionInfo.Endpoint, "EmbyFileCleaner", device, Program.GetAssemblyVersion(), cryptoProvider);
-                var passwordMd5 = ConnectService.GetConnectPasswordMd5(configLocal.ConnectionInfo.Password ?? string.Empty, cryptoProvider);
+                string passwordMd5 = ConnectService.GetConnectPasswordMd5(configLocal.ConnectionInfo.Password ?? string.Empty, cryptoProvider);
                 var authResult = await client.AuthenticateUserAsync(configLocal.ConnectionInfo.Username, passwordMd5);
                 client = new ApiClient(logger, configLocal.ConnectionInfo.Endpoint, authResult.AccessToken, cryptoProvider);
             }
